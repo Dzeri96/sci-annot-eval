5c7279057c43e53b9110bc40fabc66494868c6c1
Filip Darmanovic <e1527089@student.tuwien.ac.at>
2021-11-18 00:35:09 +0100

    Changed setup.py to work properly I guess

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
-def setup(
+from setuptools import setup
+
+setup(
     name='sci-annot-eval',
     version='0.0.1',
     description='The evaluation component of the sci-annot framework',
@@ -11,5 +13,4 @@ def setup(
     keywords=['sci-annot', 'object', 'detection', 'evaluation'],
     python_requires='>=3.9, <4',
     install_requires=['numpy==1.21.4', 'lapsolver==1.1.0']
-):
-    pass
+)
