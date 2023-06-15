b2ba43fc0142ed1d0620fc7d79fa1268af174039
Dzeri96 <dzeri96@proton.me>
2023-06-15 21:38:58 +0200

    Added sci_annot_exporter tests

--- sci_annot_eval/common/bounding_box.py
+++ sci_annot_eval/common/bounding_box.py
@@ -1,6 +1,7 @@
 from enum import Enum
 from abc import ABC, abstractmethod
 
+# TODO: Change type in BoundingBox constructor to this enum
 class TargetType(Enum):
     FIGURE = 'Figure'
     TABLE = 'Table'
