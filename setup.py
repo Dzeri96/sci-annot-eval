18ac16ae51b47de4343aeb26dd0d614c5cf568fb
Dzeri96 <dzeri96@proton.me>
2023-08-06 22:31:53 +0200

    Bumped pillow to 9.3.0 and ver to 0.0.9

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='sci_annot_eval',
-    version='0.0.8',
+    version='0.0.9',
     description='The evaluation component of the sci-annot framework',
     author='Dzeri96',
     classifiers=[
@@ -17,7 +17,7 @@ setup(
     ],
     keywords=['sci-annot', 'object', 'detection', 'evaluation'],
     python_requires='>=3.9, <4',
-    install_requires=['coloredlogs==15.0.1', "humanfriendly==10.0; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4'", 'lapsolver==1.1.0', 'mypy==1.3.0', "mypy-extensions==1.0.0; python_version >= '3.5'", 'numpy==1.22.3', 'opencv-python==4.5.5.64', 'pandas==1.4.2', 'pdf2image==1.16.0', "pillow==9.1.0; python_version >= '3.7'", 'pyarrow==7.0.0', "python-dateutil==2.8.2; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'", 'pytz==2022.1', "six==1.16.0; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'", "tomli==2.0.1; python_version < '3.11'", "typing-extensions==4.6.3; python_version >= '3.7'"],
+    install_requires=['coloredlogs==15.0.1', "humanfriendly==10.0; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4'", 'lapsolver==1.1.0', 'mypy==1.3.0', "mypy-extensions==1.0.0; python_version >= '3.5'", 'numpy==1.22.3', 'opencv-python==4.5.5.64', 'pandas==1.4.2', 'pdf2image==1.16.0', 'pillow==9.3.0', 'pyarrow==7.0.0', "python-dateutil==2.8.2; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'", 'pytz==2022.1', "six==1.16.0; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'", "tomli==2.0.1; python_version < '3.11'", "typing-extensions==4.6.3; python_version >= '3.7'"],
     packages=find_packages(),
     entry_points={
         'console_scripts': [
